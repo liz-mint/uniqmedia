d6182767164d5cd2eb7000721e9004807646f5e5
lizagunina <lizagunina21@gmail.com>
2019-11-14 21:21:56 +0700

    adaptive

---
+++ dist/js/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+    //smoth scroll and page-up
+    /* $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.page-up').fadeIn();
+        } else {
+            $('.page-up').fadeOut();
+        }
+    }); */
+
+    $("a[href^='#']").click(function (e) {
+        var _href = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top - $('.header').height()
+        });
+        e.preventDefault();
+        return false;
+    });
+
+    // Set iframe attributes when the show instance method is called
+    $("#videoModal").on("show.bs.modal", function (event) {
+        let button = $(event.relatedTarget); // Button that triggered the modal
+        let url = button.data("video"); // Extract url from data-video attribute
+
+        $(this).find("iframe").attr({
+            src: url,
+            allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        });
+        $(this).find("iframe").css('width', '100%');
+    });
+    // Remove iframe attributes when the modal has finished being hidden from the user
+    $("#videoModal").on("hidden.bs.modal", function () {
+        $("#videoModal iframe").removeAttr("src allow");
+    });
+
+    //case and statistic animation
+    $('.case__block').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeInDown',
+        offset: 100
+    });
+    $('.statistics__text').addClass("hidden").viewportChecker({
+        classToAdd: 'visible statistics__text_active',
+        offset: 100
+    });
+
+
+    //counter increment and arrow-up-button
+    let show = true;
+    const countbox = ".statistics__block";
+    $(window).on("scroll load resize", function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.page-up').fadeIn();
+        } else {
+            $('.page-up').fadeOut();
+        }
+
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        let w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        let e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        let w_height = $(window).height(); // Высота окна браузера
+        let d_height = $(document).height(); // Высота всего документа
+        let e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            // $('.counter-block__number').css('opacity', '1');
+            $('.statistics__square').spincrement({
+                thousandSeparator: ",",
+                duration: 1200
+            });
+            show = false;
+        }
+    });
+})
--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 
 // Static server
-gulp.task('server', function() {
+gulp.task('server', function () {
     browserSync.init({
         server: {
             baseDir: "dist"
@@ -17,54 +17,60 @@ gulp.task('server', function() {
     gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
-gulp.task('styles', function(){
+gulp.task('styles', function () {
     return gulp.src("src/sass/**/*.+(sass|scss)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
         .pipe(rename({
             prefix: "",
             suffix: ".min",
         }))
         .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
         .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     gulp.watch("src/sass/**/*.+(sass|scss|css)", gulp.parallel("styles"))
     gulp.watch("src/*.html").on("change", gulp.parallel('html'));
 });
 
-gulp.task('html', function() {
+gulp.task('html', function () {
     return gulp.src("src/*.html")
-        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
         .pipe(gulp.dest("dist/"));
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
     return gulp.src("src/js/**/*.js")
         .pipe(gulp.dest("dist/js"));
 });
 
-gulp.task('fonts', function() {
+gulp.task('fonts', function () {
     return gulp.src("src/fonts/**/*")
         .pipe(gulp.dest("dist/fonts"));
 });
 
-gulp.task('icons', function() {
+gulp.task('icons', function () {
     return gulp.src("src/icons/**/*")
         .pipe(gulp.dest("dist/icons"));
 });
 
-gulp.task('images', function() {
+gulp.task('images', function () {
     return gulp.src("src/img/**/*")
         .pipe(imagemin())
         .pipe(gulp.dest("dist/img"));
 });
 
-gulp.task('mailer', function() {
+gulp.task('mailer', function () {
     return gulp.src("src/mailer/**/*")
         .pipe(gulp.dest("dist/mailer"));
 });
 
-gulp.task('default', gulp.parallel('watch','server', 'styles', 'html','scripts', 'fonts', 'icons', 'images', 'mailer'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'fonts', 'icons', 'images', 'mailer'));
